1e7e8ea7c99390be49072c94f93926dbd491d129
edianannink <e_dian@live.nl>
2025-12-04 23:28:03 +0100

    2024 day 16 part 2

--- src/year2024/day16.rs
+++ src/year2024/day16.rs
@@ -1,16 +1,17 @@
 use crate::year2024::examples;
+use std::collections::{HashSet, VecDeque};
 use std::{cmp::Reverse, collections::BinaryHeap};
 
 const DIRECTIONS: &[(isize, isize)] = &[(-1, 0), (0, 1), (1, 0), (0, -1)];
 
 pub fn solution() -> (usize, usize) {
-    let input = std::fs::read_to_string("./src/input/day16.txt")
-        .unwrap_or_else(|_| examples::DAY16.to_string());
+    let input = std::fs::read_to_string("./src/year2024/input/day16.txt")
+        .unwrap_or_else(|_| examples::DAY16_1.to_string());
 
-    (part1(&input), 0)
+    solve(&input)
 }
 
-pub fn part1(input: &str) -> usize {
+pub fn solve(input: &str) -> (usize, usize) {
     let maze: Vec<Vec<char>> = input.lines().map(|l| l.chars().collect()).collect();
     let mut start = (0, 0);
     let mut end = (0, 0);
@@ -28,19 +29,17 @@ pub fn part1(input: &str) -> usize {
     dijkstra(&maze, start, end)
 }
 
-fn dijkstra(maze: &[Vec<char>], start: (usize, usize), end: (usize, usize)) -> usize {
+fn dijkstra(maze: &[Vec<char>], start: (usize, usize), end: (usize, usize)) -> (usize, usize) {
     let (rows, cols) = (maze.len(), maze[0].len());
     let mut heap = BinaryHeap::new();
     let mut dist = vec![vec![vec![usize::MAX; 4]; cols]; rows];
+    let mut predecessors: Vec<Vec<Vec<Vec<(usize, usize, usize)>>>> =
+        vec![vec![vec![Vec::new(); 4]; cols]; rows];
 
     dist[start.0][start.1][1] = 0;
     heap.push(Reverse((0, start.0, start.1, 1)));
 
     while let Some(Reverse((cost, cr, cc, dir))) = heap.pop() {
-        if (cr, cc) == end {
-            return cost;
-        }
-
         if cost > dist[cr][cc][dir] {
             continue;
         }
@@ -55,7 +54,10 @@ fn dijkstra(maze: &[Vec<char>], start: (usize, usize), end: (usize, usize)) -> u
                 let new_cost = cost + 1;
                 if new_cost < dist[nr][nc][dir] {
                     dist[nr][nc][dir] = new_cost;
+                    predecessors[nr][nc][dir] = vec![(cr, cc, dir)];
                     heap.push(Reverse((new_cost, nr, nc, dir)));
+                } else if new_cost == dist[nr][nc][dir] {
+                    predecessors[nr][nc][dir].push((cr, cc, dir));
                 }
             }
         }
@@ -64,10 +66,39 @@ fn dijkstra(maze: &[Vec<char>], start: (usize, usize), end: (usize, usize)) -> u
             let new_cost = cost + 1000;
             if new_cost < dist[cr][cc][new_dir] {
                 dist[cr][cc][new_dir] = new_cost;
+                predecessors[cr][cc][new_dir] = vec![(cr, cc, dir)];
                 heap.push(Reverse((new_cost, cr, cc, new_dir)));
+            } else if new_cost == dist[cr][cc][new_dir] {
+                predecessors[cr][cc][new_dir].push((cr, cc, dir));
             }
         }
     }
 
-    0
+    let min_cost = (0..DIRECTIONS.len())
+        .map(|d| dist[end.0][end.1][d])
+        .min()
+        .unwrap();
+
+    let mut queue = VecDeque::new();
+    let mut visited = HashSet::new();
+    let mut tiles = HashSet::new();
+
+    (0..DIRECTIONS.len()).for_each(|dir| {
+        if dist[end.0][end.1][dir] == min_cost {
+            queue.push_back((end.0, end.1, dir));
+            visited.insert((end.0, end.1, dir));
+        }
+    });
+
+    while let Some((r, c, dir)) = queue.pop_front() {
+        tiles.insert((r, c));
+
+        for &(pr, pc, pdir) in &predecessors[r][c][dir] {
+            if visited.insert((pr, pc, pdir)) {
+                queue.push_back((pr, pc, pdir));
+            }
+        }
+    }
+
+    (min_cost, tiles.len())
 }
