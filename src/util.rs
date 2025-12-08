a1bfa93ec706859ff0cf90fc7a29b532cf919722
edianannink <e_dian@live.nl>
2025-12-09 00:06:00 +0100

    This one was far from easy for me due to my limited knowledge of disjoint-set data structures and minimum spanning forest algorithms. I finally solved it after a clear structure and using a basic union find data structure in combination with a similar approach to Kruskal's algorithm.

--- src/util.rs
+++ src/util.rs
@@ -9,3 +9,73 @@ pub(crate) fn print_matrix<T: std::fmt::Display>(matrix: &Vec<Vec<T>>) {
     }
     println!();
 }
+
+#[derive(Debug)]
+pub(crate) struct JunctionBox {
+    pub(crate) x: usize,
+    pub(crate) y: usize,
+    pub(crate) z: usize,
+}
+
+impl JunctionBox {
+    pub(crate) fn new(str: &str) -> Self {
+        let coords: Vec<usize> = str
+            .split(",")
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect();
+        Self {
+            x: coords[0],
+            y: coords[1],
+            z: coords[2],
+        }
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub(crate) struct Edge {
+    pub(crate) u: usize,
+    pub(crate) v: usize,
+    pub(crate) weight: usize,
+}
+
+impl Edge {
+    pub(crate) fn new(u: usize, v: usize, weight: usize) -> Self {
+        Self { u, v, weight }
+    }
+}
+
+#[derive(Debug)]
+pub(crate) struct UnionFind {
+    parent: Vec<usize>,
+}
+
+impl UnionFind {
+    pub(crate) fn new(n: usize) -> Self {
+        Self {
+            parent: (0..n).collect(),
+        }
+    }
+
+    pub(crate) fn find(&mut self, x: usize) -> usize {
+        if self.parent[x] != x {
+            self.parent[x] = self.find(self.parent[x]);
+        }
+        self.parent[x]
+    }
+
+    pub(crate) fn union(&mut self, x: usize, y: usize) -> bool {
+        let root_a = self.find(x);
+        let root_b = self.find(y);
+        if root_a != root_b {
+            self.parent[root_b] = root_a;
+            true
+        } else {
+            false
+        }
+    }
+
+    pub(crate) fn fully_connected(&mut self) -> bool {
+        let root = self.find(0);
+        (1..self.parent.len()).all(|i| self.find(i) == root)
+    }
+}
--- src/year2025/day8.rs
+++ src/year2025/day8.rs
@@ -1,53 +1,66 @@
+use crate::util::{Edge, JunctionBox, UnionFind};
+use crate::year2025::examples;
 use std::collections::HashMap;
 
-use crate::year2025::examples;
-
-#[derive(Debug, Eq, PartialEq, Hash)]
-struct Box {
-    x: isize,
-    y: isize,
-    z: isize,
-}
-
-impl Box {
-    fn new(coord_str: &str) -> Self {
-        let coords: Vec<isize> = coord_str
-            .split(",")
-            .map(|b| b.parse::<isize>().unwrap())
-            .collect();
-        Self {
-            x: coords[0],
-            y: coords[1],
-            z: coords[2],
-        }
-    }
-
-    fn euclidean_distance(&self, other: &Box) -> usize {
-        let float = (((self.x - other.x) as f64).powi(2)
-            + ((self.y - other.y) as f64).powi(2)
-            + ((self.z - other.z) as f64).powi(2))
-        .sqrt();
-        float.round() as usize
-    }
-}
+const CONNECTIONS: usize = 10;
 
 pub fn solution() -> (String, String) {
-    let input = std::fs::read_to_string("./src/year2025/input/day9.txt")
+    let input = std::fs::read_to_string("./src/year2025/input/day8.txt")
         .unwrap_or_else(|_| examples::DAY8.to_string());
 
-    let mut boxes: Vec<Box> = input.lines().map(Box::new).collect();
+    let boxes: Vec<JunctionBox> = input.lines().map(JunctionBox::new).collect();
 
-    for a in &boxes {
-        let mut vec = Vec::new();
-        for b in &boxes {
-            vec.push((a.euclidean_distance(b), b));
+    solve(&boxes)
+}
+
+fn solve(boxes: &[JunctionBox]) -> (String, String) {
+    let size = boxes.len();
+
+    let mut edges = Vec::new();
+    for i in 0..size {
+        for j in (i + 1)..size {
+            let dx = boxes[j].x as isize - boxes[i].x as isize;
+            let dy = boxes[j].y as isize - boxes[i].y as isize;
+            let dz = boxes[j].z as isize - boxes[i].z as isize;
+            let weight = (dx * dx + dy * dy + dz * dz) as usize;
+            edges.push(Edge::new(i, j, weight))
         }
-        vec.sort_by_key(|k| k.0);
-        for line in vec {
-            println!("{line:?}");
-        }
-        println!();
+    }
+    edges.sort_by_key(|k| k.weight);
+
+    (
+        part1(&edges, size).to_string(),
+        part2(boxes, &edges, size).to_string(),
+    )
+}
+
+fn part1(edges: &[Edge], size: usize) -> usize {
+    let mut uf = UnionFind::new(size);
+    for edge in edges.iter().take(CONNECTIONS) {
+        uf.union(edge.u, edge.v);
     }
 
-    (String::new(), String::new())
+    let mut groups: HashMap<usize, Vec<usize>> = HashMap::new();
+    for node in 0..size {
+        let root = uf.find(node);
+        groups.entry(root).or_default().push(node);
+    }
+
+    let mut values: Vec<usize> = groups.clone().into_values().map(|v| v.len()).collect();
+    values.sort();
+
+    values.iter().rev().take(3).product::<usize>()
+}
+
+fn part2(boxes: &[JunctionBox], edges: &[Edge], size: usize) -> usize {
+    let mut uf = UnionFind::new(size);
+    let mut final_edge = &Edge::default();
+
+    for edge in edges {
+        if uf.union(edge.u, edge.v) && uf.fully_connected() {
+            final_edge = edge;
+            break;
+        }
+    }
+    boxes[final_edge.u].x * boxes[final_edge.v].x
 }
