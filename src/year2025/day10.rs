a7859752ee81333355b6cae43f8c8c685b036df4
edianannink <e_dian@live.nl>
2025-12-11 00:38:05 +0100

    day 10 part 2

--- src/year2025/day10.rs
+++ src/year2025/day10.rs
@@ -1,11 +1,12 @@
-use std::collections::{HashSet, VecDeque};
-
 use crate::year2025::examples;
+use good_lp::{Solution, SolverModel, constraint, default_solver, variable, variables};
+use std::collections::{HashSet, VecDeque};
 
 #[derive(Debug, Clone)]
 struct Machine {
     lights: usize,
     buttons: Vec<usize>,
+    joltage: Vec<usize>,
 }
 
 impl Machine {
@@ -36,7 +37,18 @@ impl Machine {
             buttons.push(schematic);
         }
 
-        Self { lights, buttons }
+        let joltage = parts
+            .last()
+            .unwrap()
+            .split(",")
+            .map(|str| str.parse().unwrap())
+            .collect::<Vec<usize>>();
+
+        Self {
+            lights,
+            buttons,
+            joltage,
+        }
     }
 }
 
@@ -46,11 +58,12 @@ pub fn solution() -> (String, String) {
 
     let machines: Vec<Machine> = input.lines().map(Machine::new).collect();
 
-    (solve(&machines).to_string(), String::from("Python"))
+    solve(&machines)
 }
 
-fn solve(machines: &[Machine]) -> usize {
-    let mut result = 0;
+fn solve(machines: &[Machine]) -> (String, String) {
+    let mut part1 = 0;
+    let mut part2 = 0;
 
     for machine in machines {
         let mut min_press = usize::MAX;
@@ -73,8 +86,37 @@ fn solve(machines: &[Machine]) -> usize {
             }
         }
 
-        result += min_press;
+        part1 += min_press;
+        part2 += solve_lp(machine);
     }
 
-    result
+    (part1.to_string(), part2.to_string())
+}
+
+fn solve_lp(machine: &Machine) -> usize {
+    let mut vars = variables!();
+    let button_vars: Vec<_> = (0..machine.buttons.len())
+        .map(|_| vars.add(variable().integer().min(0)))
+        .collect();
+
+    let mut problem = vars
+        .minimise(button_vars.iter().sum::<good_lp::Expression>())
+        .using(default_solver);
+
+    for (counter_idx, &joltage) in machine.joltage.iter().enumerate() {
+        let expr = button_vars
+            .iter()
+            .enumerate()
+            .filter(|(j, _)| (machine.buttons[*j] & (1 << counter_idx)) != 0)
+            .map(|(_, &var)| var)
+            .sum::<good_lp::Expression>();
+
+        problem = problem.with(constraint!(expr == joltage as i32));
+    }
+
+    let solution = problem.solve().unwrap();
+    button_vars
+        .iter()
+        .map(|&var| solution.value(var).round() as usize)
+        .sum()
 }
