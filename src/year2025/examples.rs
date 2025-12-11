0768995328478b959d0e3f3332b4fa77978c3728
edianannink <edian.annink@nedap.com>
2025-12-11 12:42:30 +0100

    day 11 part 1

--- src/year2025/examples.rs
+++ src/year2025/examples.rs
@@ -99,3 +99,28 @@ pub(crate) const DAY9: &str = r#"7,1
 pub(crate) const DAY10: &str = r#"[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
 [...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
 [.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}"#;
+
+pub(crate) const DAY11_1: &str = r#"aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out"#;
+
+pub(crate) const DAY11_2: &str = r#"svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out"#;
